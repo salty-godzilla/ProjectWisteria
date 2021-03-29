1ebdb11710240abbcb2367108a6b2ecf9361a8a6
nitenook <admin@alterbaum.net>
2021-03-29 23:30:36 +0900

    Added wireframe mode for debugging

---
+++ scripts/DebugDraw.cs
@@ -0,0 +1,26 @@
+﻿using Godot;
+
+namespace ProjectWisteria
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    // ReSharper disable once UnusedType.Global
+    public class DebugDraw : Node
+    {
+        public override void _Ready()
+        {
+            VisualServer.SetDebugGenerateWireframes(true);
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            if (@event is InputEventKey {Pressed: true, Scancode: (uint) KeyList.F4})
+            {
+                var vp = GetViewport()!;
+                vp.DebugDraw =
+                    vp.DebugDraw == Viewport.DebugDrawEnum.Disabled
+                        ? Viewport.DebugDrawEnum.Wireframe
+                        : Viewport.DebugDrawEnum.Disabled;
+            }
+        }
+    }
+}
